3062ead7f0157ae5ea8700af421b96b2030e0b8f
Mickae Truman <mtruman@nyit.edu>
2019-05-10 02:12:13 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,17 +1,9 @@
 class AddGradeBirthdateToStudents < ActiveRecord::Migration[5.1]
   
 def change  
-  add_column :
-    t.integer :grade
-    t.string :birthdate
+  add_column :grade :integer, :birthdate :string
+   # t.integer :grade, t.string :birthdate
 end  
 
 end
 
-end
-
-class AddFavoriteFoodToArtists < ActiveRecord::Migration
-  def change
-    add_column :artists, :favorite_food, :string
-  end
-end
