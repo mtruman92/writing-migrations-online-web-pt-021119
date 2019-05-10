d88f1ed6bc6f8188b1b7ede97f62f426f206a8d7
Mickae Truman <mtruman@nyit.edu>
2019-05-10 02:16:13 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -3,6 +3,13 @@ class CreateStudents < ActiveRecord::Migration[5.1]
 def change  
   create_table :students do |t|
     t.string :name
+    
+    sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+  name TEXT)
+SQL
+ 
+ActiveRecord::Base.connection.execute(sql)
 end  
 
 end
