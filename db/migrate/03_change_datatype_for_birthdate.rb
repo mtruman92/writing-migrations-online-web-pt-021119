872a1d0baa2e9f484ce7ec4e1233dfadd7f3bb02
Mickae Truman <mtruman@nyit.edu>
2019-05-10 02:06:02 +0000

    Automatically backed up by Learn

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,11 @@
+class CreateStudents < ActiveRecord::Migration[5.1]
+  
+def change  
+  change_column(students,birthdate,datetime) :students do |t|
+    t.integer :grade
+    t.string :birthdate
+end  
+
+end
+
+end
